0cc52aee6b621d08158f81f4b3804ed7d4000f47
TranHongQuang <quanghulk81@gmail.com>
2026-06-09 16:06:14 +0700

    Update api TechnicalCategory

--- coffee_support_project/routes/api.php
+++ coffee_support_project/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CoffeeFarmController;
 use App\Http\Controllers\Api\CultivationLogController;
+use App\Http\Controllers\Api\TechnicalCategoryController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -13,4 +14,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('coffee-farms', CoffeeFarmController::class);
     Route::apiResource('cultivation-logs', CultivationLogController::class);
+    Route::apiResource('technical-categories', TechnicalCategoryController::class);
 });
