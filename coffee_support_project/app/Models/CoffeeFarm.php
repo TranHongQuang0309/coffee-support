899d8b896933c201bd512e3d4da6a7b7b7202a60
TranHongQuang <quanghulk81@gmail.com>
2026-06-09 10:34:55 +0700

    update model CoffeeFarm

--- coffee_support_project/app/Models/CoffeeFarm.php
+++ coffee_support_project/app/Models/CoffeeFarm.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
 use App\Models\CultivationLog;
 use App\Models\DiagnosisRequest;
 use App\Models\Question;
+use App\Models\WeatherData;
+
 class CoffeeFarm extends Model
 {
-    //
-     protected $fillable = [
+    protected $fillable = [
         'user_id',
         'farm_name',
         'area',
@@ -31,17 +33,22 @@ class CoffeeFarm extends Model
     }
 
     public function cultivationLogs()
-{
-    return $this->hasMany(CultivationLog::class);
-}
+    {
+        return $this->hasMany(CultivationLog::class);
+    }
 
-public function diagnosisRequests()
-{
-    return $this->hasMany(DiagnosisRequest::class);
-}
+    public function diagnosisRequests()
+    {
+        return $this->hasMany(DiagnosisRequest::class);
+    }
 
-public function questions()
-{
-    return $this->hasMany(Question::class);
-}
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function weatherData()
+    {
+        return $this->hasMany(WeatherData::class);
+    }
 }
