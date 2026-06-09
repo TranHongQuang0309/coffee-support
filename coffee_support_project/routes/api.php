4cc63711def81894aefcb0b4dae4879dd6bc866b
TranHongQuang <quanghulk81@gmail.com>
2026-06-09 15:57:01 +0700

    update api

--- coffee_support_project/routes/api.php
+++ coffee_support_project/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CoffeeFarmController;
+use App\Http\Controllers\Api\CultivationLogController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,4 +12,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('coffee-farms', CoffeeFarmController::class);
+    Route::apiResource('cultivation-logs', CultivationLogController::class);
 });
